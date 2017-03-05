7a3379c1dbfa078b8044133c34555d43e88cf058
Valzavator <maxsmv1998@gmail.com>
2017-03-05 20:30:57 +0200

    travis

--- install-check.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-set -ex
-wget https://github.com/libcheck/check/releases/download/0.11.0/check-0.11.0.tar.gz
-tar -xzvf check-0.11.0.tar.gz
-cd check-0.11.0 && ./configure --prefix=/usr && make && sudo make install
