ca1c43347ff52f3970f7580e3cdecbcd6eeb7539
Valzavator <maxsmv1998@gmail.com>
2017-03-05 21:14:22 +0200

    test

--- tests/main.c
+++ tests/main.c
@@ -16,22 +16,22 @@ START_TEST (CSVToList_string_headOfList);
     const char * name = Student_getName(List_get(List_elementAt(list, 0)));
     const char * surname = Student_getSurname(List_get(List_elementAt(list, 0)));
     int age = Student_getAge(List_get(List_elementAt(list, 0)));
-    // float score = Student_getScore(List_get(List_elementAt(list, 0)));
+    float score = Student_getScore(List_get(List_elementAt(list, 0)));
     
     ck_assert_str_eq(name, "Vasya");
     ck_assert_str_eq(surname, "Petrenko");
     ck_assert_int_eq(age, 19);
-    // ck_assert_float_eq(score, 4.1);
+    ck_assert_float_eq(score, 4.1);
     
     name = Student_getName(List_get(List_elementAt(list, 1)));
     surname = Student_getSurname(List_get(List_elementAt(list, 1)));
     age = Student_getAge(List_get(List_elementAt(list, 1)));
-    // score = Student_getScore(List_get(List_elementAt(list, 1)));
+    score = Student_getScore(List_get(List_elementAt(list, 1)));
     
     ck_assert_str_eq(name, "Paul");
     ck_assert_str_eq(surname, "Kozlov");
     ck_assert_int_eq(age, 20);
-    // ck_assert_float_eq(score, 3.7);
+    ck_assert_float_eq(score, 3.7);
 
     List_clear(list);
 }
